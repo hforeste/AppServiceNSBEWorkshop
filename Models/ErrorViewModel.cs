16c122970df206c1834c9383201f96dd5fa7474d
Hawk Foreste <hawfor@microsoft.com>
2023-03-25 10:54:17 -0700

    Create MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyNSBE_MVC_App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
